c39343dd3839baa6502b5a8e7887e634f9746ea6
Melfely4215 <bug362@outlook.com>
2025-11-13 17:15:35 -0600

    Swapped to a pico 2 rather than pico 2 w so I can use the SWD debug system.
    This is also sets up the project correctly to support SWD debug.

---
+++ p2.cpp
@@ -0,0 +1,177 @@
+#include <stdio.h>
+#include <string>
+#include "pico/stdlib.h"
+#include "hardware/pwm.h"
+#include "pico/multicore.h"
+#include "hardware/watchdog.h"
+#include "PWM.h"
+#include "GPIO.h"
+#include "Sensor.h"
+#include "DriveTrain.h"
+#include <atomic>
+
+#pragma region 
+volatile std::atomic<int> mode = 0;
+static float workTime = 0;
+uint64_t buttonHoldStart_us; 
+GPIO::BUTTON mainButton(15, false);
+alarm_id_t alarmHoldID;
+
+#pragma region Function Headers
+void mainButtonModeChange_callback(uint pin, uint32_t events);
+void mainButtonHold_callback(uint pin, uint32_t events);
+void mainButtonInterupt_init();
+
+int64_t alarmHoldRestart_callback(alarm_id_t event, void* USERDATA);
+
+void core1_main(); 
+
+#pragma endregion
+
+#pragma region Main
+int main()
+{
+    
+    GPIO::LED redLed(28);
+    PWM::LED blueLed(26);
+    PWM::LED greenLed(27);
+    uint64_t workStart_us = 0;
+    float idleFadeCycle = 0;
+    float lowPowerWarnCycle = 0;
+
+    //Init this I guess? It comes in the project
+    stdio_init_all();
+    
+    //Launch core1
+    multicore_launch_core1(core1_main);
+
+    //Wait for core1 to fully init
+    multicore_fifo_pop_blocking();
+
+    if (mainButton.GetState() == 0) {
+        for (int i = 0; i < 200; i++) {
+            redLed.ToggleEvery(.1);
+            blueLed.ToggleEvery(.1);
+            greenLed.ToggleEvery(.1);
+
+            sleep_ms(10);
+        }
+        redLed.SetState(0);
+        blueLed.SetState(0);
+        greenLed.SetState(0);
+    } else {
+        for(int i = 0; i < 100; i++) {
+            redLed.ToggleEvery(.2);
+            sleep_ms(10);
+        }
+        watchdog_reboot(0,0,0);
+    }
+
+    while (true) {
+
+        
+        if (mode % 2 == 0) {
+        #pragma region Pause Mode
+
+            if(workTime < 45) {
+                redLed.SetState(0);
+                greenLed.FadeUp(500, 1, mode);
+                if (mode % 2 != 0) continue;
+                greenLed.FadeDown(500, 1, mode);
+                blueLed.SetState(0);
+            }else if (workTime >= 45 && workTime < 55){
+                redLed.SetState(0);
+                blueLed.FadeUp(500, 1, mode);
+                if (mode % 2 != 0) continue;
+                blueLed.FadeDown(500, 1, mode);
+                greenLed.SetState(0);
+            } else if (workTime >= 55 && workTime < 60) 
+            {
+                workStart_us = time_us_64(); //Start accuminlating workTime after 55 seconds in anymode, so it can restart after 60 (or 5 seconds of red light)
+                if (idleFadeCycle <= .5) {
+                    blueLed.SetDuty(idleFadeCycle * 2);
+                } else if (idleFadeCycle < 1) {
+                    blueLed.SetDuty(1.0f - ( (idleFadeCycle - 0.5f) * 2.0f));
+                } else {
+                    idleFadeCycle = 0;
+                }
+
+                redLed.ToggleEvery(.05);
+                sleep_ms(10);
+                workTime += (float)(time_us_64() - workStart_us) / 1000000.0f;
+                idleFadeCycle += (float)(time_us_64() - workStart_us) / 1000000.0f;
+                lowPowerWarnCycle += (float)(time_us_64() - workStart_us) / 1000000.0f;
+                
+            } else {
+                break;
+            }
+        #pragma endregion
+        } else {
+        #pragma region Work Mode
+           
+            workStart_us = time_us_64();
+            
+            if (workTime < 45) {
+                greenLed.SetState(1);
+            } else if (workTime >= 45 && workTime < 55) {
+                greenLed.SetState(0);
+                blueLed.SetState(1);
+            } else if (workTime >= 55 && workTime < 60) {
+                greenLed.SetState(0);
+                blueLed.SetState(1);
+                redLed.ToggleEvery(.05);
+            } else {
+                break;
+            }
+            sleep_ms(10);
+            workTime += (float)(time_us_64() - workStart_us) / 1000000.0f;
+            lowPowerWarnCycle += (float)(time_us_64() - workStart_us) / 1000000.0f;
+        }
+        #pragma endregion
+    }
+
+    watchdog_reboot(0, 0, 100);
+}
+#pragma endregion
+
+#pragma region Functions
+void mainButtonHold_callback(uint pin, uint32_t events) 
+{
+    buttonHoldStart_us = time_us_64();
+    //Start waiting for the edge fall callback.
+    alarm_pool_init_default();
+    alarmHoldID = add_alarm_in_ms(3000, &alarmHoldRestart_callback, NULL, true);
+    gpio_set_irq_enabled_with_callback(mainButton.GetPin(), GPIO_IRQ_EDGE_FALL, true, &mainButtonModeChange_callback);
+    
+}
+
+void mainButtonModeChange_callback(uint pin, uint32_t events) {
+    mode++;
+    cancel_alarm(alarmHoldID);
+    gpio_set_irq_enabled_with_callback(mainButton.GetPin(), GPIO_IRQ_EDGE_RISE, true, &mainButtonHold_callback);
+
+}
+
+void mainButtonInterupt_init() {
+    
+    gpio_set_irq_enabled_with_callback(mainButton.GetPin(), GPIO_IRQ_EDGE_RISE, true, &mainButtonHold_callback);
+}
+
+int64_t alarmHoldRestart_callback(alarm_id_t event, void* USERDATA) {
+    if (mainButton.IsPressed()) {
+        watchdog_reboot(0,0,0);
+    }
+    return 1;
+}
+
+void core1_main() {
+
+    mainButtonInterupt_init();//Init the mainButton interupt chain
+
+    multicore_fifo_push_blocking(72);//Let Core0 know I am started
+
+    while (true) {
+        __wfi(); //Wait for interupts. 
+    }
+}
+#pragma endregion
